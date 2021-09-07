af7cca540e4c2ce952961ba15fd6d0de7e63e98f
Hilkiah Makemo <hilkiahmakemo@Hilkiahs-MacBook-Pro.local>
2021-09-07 09:09:54 -0600

    updated resources

--- src/Http/Resources/ExtendsResource.php
+++ src/Http/Resources/ExtendsResource.php
@@ -1,50 +1,41 @@
 <?php
 
-
 namespace Core\Http\Resources;
-
 
 use Illuminate\Support\Str;
 
 trait ExtendsResource
 {
-   /**
-    * @param $request
-    * @param $resource
-    * @return mixed
-    */
-   protected function getWithRelation($request, $resource)
-   {
-      if(!is_array($with = $request->with)){
-         $with = explode(",", $with);
-      }
-      
-      foreach($with as $w){
-         if(Str::contains($w, '.')){
-            $resource = $this->load($w);
-         } else {
-            $resource[$w] = $this->{Str::camel($w)};
-         }
-      }
-      
-      return $resource;
-   }
-   
-   /**
-    * @param $request
-    * @param $resource
-    * @return mixed
-    */
-   protected function getAllRelations($request, $resource)
-   {
-      foreach($resource as $k => $val){
-         if(!Str::endsWith($k, '_id')){
-            $resource[$k] = $val;
-            continue;
-         }
-         $with = (string)Str::of($k)->before('_id')->camel()->ucfirst();
-         $resource[$with] = $this->{Str::camel($with)};
-      }
-      return $resource;
-   }
+    /**
+     * @param $request
+     * @param $resource
+     * @return mixed
+     */
+    protected function getWithRelation($request, $resource)
+    {
+        $with = is_array($request->with) ?
+            $request->with : explode(",", $request->with);
+
+        $resource += [$this->mergeWhen(count($with) > 0, $this->load($with))];
+
+        return $resource;
+    }
+
+    /**
+     * @param $request
+     * @param $resource
+     * @return mixed
+     */
+    protected function getAllRelations($request, $resource)
+    {
+        foreach ($resource as $k => $val) {
+            if (!Str::endsWith($k, '_id')) {
+                $resource[$k] = $val;
+                continue;
+            }
+            $with = (string)Str::of($k)->before('_id')->camel()->ucfirst();
+            $resource[$with] = $this->{Str::camel($with)};
+        }
+        return $resource;
+    }
 }
